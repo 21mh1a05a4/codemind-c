53677312fd0d42c4feefda38fbb2bce475a5bb38
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-06-28 21:01:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,sum=0,avg;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         sum+=a[i];     }     avg=sum/n;     int c=0;     for(i=0;i<n;i++)     {         if(a[i]<=avg)         {             c++;         }     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,sum=0,avg;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum+=a[i];
+    }
+    avg=sum/n;
+    int c=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
