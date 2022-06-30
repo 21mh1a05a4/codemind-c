1af790e1f0d4d187f7381b30e46fa4b6f9b8f605
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-06-30 22:03:02 +0530

    #include<stdio.h> int main() {     int r,c;     scanf("%d%d",&r,&c);     int a[100][100],i,j,sum=0;     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             if(i!=0 && j!=0)            {                if(i!=(r-1) && j!=(c-1))                {                    sum+=a[i][j];                }            }         }     }     printf("%d",sum);
    }

---
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int a[100][100],i,j,sum=0;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(i!=0 && j!=0)
+           {
+               if(i!=(r-1) && j!=(c-1))
+               {
+                   sum+=a[i][j];
+               }
+           }
+        }
+    }
+    printf("%d",sum);
+    
+}
