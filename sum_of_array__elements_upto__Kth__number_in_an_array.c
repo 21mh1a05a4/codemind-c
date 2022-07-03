30e54bbf90952e4280705cd3baf67d6449b4a42c
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-07-03 20:40:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     int sum=0;     for(i=0;i<n;i++)     {        sum+=a[i];        if(a[i]==k)        {            break;        }     }     printf("%d",sum); }

--- sum_of_array__elements_upto__Kth__number_in_an_array.c
+++ sum_of_array__elements_upto__Kth__number_in_an_array.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,sum=0;
+    int n;
     scanf("%d",&n);
-    int arr[n],i;
+    int a[n],i;
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        scanf("%d",&a[i]);
     }
+    int k;
     scanf("%d",&k);
+    int sum=0;
     for(i=0;i<n;i++)
     {
-        if(arr[i]<=k)
-        {
-            sum+=arr[i];
-        }
+       sum+=a[i];
+       if(a[i]==k)
+       {
+           break;
+       }
     }
     printf("%d",sum);
 }
