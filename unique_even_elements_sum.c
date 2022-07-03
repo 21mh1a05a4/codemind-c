f7081e40df0fa7b1c7df15b8834d29c2a616411a
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-07-03 20:09:38 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,sum=0,j;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(a[i]==a[j] && a[i]!=-1)                 {                     a[j]=-1;                 }             }         }     }     for(i=0;i<n;i++)     {         if(a[i]!=-1)         {             if(a[i]%2==0)             {                 sum+=a[i];             }         }     }     printf("%d",sum); }

---
+++ unique_even_elements_sum.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,sum=0,j;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j] && a[i]!=-1)
+                {
+                    a[j]=-1;
+                }
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!=-1)
+        {
+            if(a[i]%2==0)
+            {
+                sum+=a[i];
+            }
+        }
+    }
+    printf("%d",sum);
+}
