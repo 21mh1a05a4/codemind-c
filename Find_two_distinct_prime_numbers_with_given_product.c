86a61f5c03b4259e961e1a3e9b4bf803036e4b67
k.varshini <97451485+21mh1a05a4@users.noreply.github.com>
2022-07-03 11:58:49 +0530

    #include<stdio.h> int prime(int num) {     int i,count=0;     for(i=1;i<=num;i++)     {         if(num%i==0)         {             count++;         }         if(count==2)         {             return 1;         }     }     return 0; } int main() {     int n,i,j,s=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i*j==n)             {                 if(prime(i)==1 && prime(j)==1)                 {                     printf("%d %d",i,j);                     s=1;                 }             }             if(s==1)             {                 break;             }         }     }     if(s==0)     {         printf("-1");     } }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -17,7 +17,7 @@ int prime(int num)
 }
 int main()
 {
-    int i,j,n,s=0;
+    int n,i,j,s=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
@@ -30,7 +30,6 @@ int main()
                     printf("%d %d",i,j);
                     s=1;
                 }
-                
             }
             if(s==1)
             {
